c940f3bfaf4f80eda96d39742554ac6d5b04a0b1
soO <444deph12@gmail.com>
2023-04-12 03:44:08 -0700

    add of api v1, port setup to 5050, fixing pep8 and commentary

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
+#!/usr/bin/python3
+""" Blueprint v1"""
+from api.v1.views.index import *
 from flask import Blueprint
 
-# Create a Blueprint instance
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import the routes
-from api.v1.views.index import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
+"""index, route /status created"""
 from api.v1.views import app_views
 from flask import jsonify
-
-# Create a route
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
