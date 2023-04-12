8fde613b2c2ba7671a0e3de7936f3977cbb24d61
root <root@pc>
2023-04-12 13:19:05 +0200

    pycodestyle fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,11 +15,11 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     objs = {
-        "amenities": storage.count("Amenity"), 
-        "cities": storage.count("City"), 
-        "places": storage.count("Place"), 
-        "reviews": storage.count("Review"), 
-        "states": storage.count("State"), 
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
         "users": storage.count("User")
     }
 
