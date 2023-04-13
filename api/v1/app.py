d01388fab9537a4bb1ad298526bb0d2a01ae2c0a
soOwasTaken <113637950+soOwasTaken@users.noreply.github.com>
2023-04-13 12:43:44 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -21,6 +21,5 @@ def not_found(error):
 if __name__ == "__main__":
     """ all ips, port 5050"""
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
-
+    port = int(os.environ.get("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
