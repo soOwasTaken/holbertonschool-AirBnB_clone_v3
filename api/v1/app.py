691a3c2cff7a990ea801c9939ca7ad6998d00b08
soOwasTaken <113637950+soOwasTaken@users.noreply.github.com>
2023-04-13 13:02:23 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """app v1 port set to 5050"""
+
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -19,7 +22,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    """ all ips, port 5050"""
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
