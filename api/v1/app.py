daef95d9a2a0ae2cc8d01bc875364fe664e2d0a3
soOwasTaken <113637950+soOwasTaken@users.noreply.github.com>
2023-04-13 12:53:33 +0200

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, jsonify
 from api.v1.views import app_views
 from models import storage
-import os
+from os import getenv
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
