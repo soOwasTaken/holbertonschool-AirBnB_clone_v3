8d3b8f34f51538a71791e0c560ea0afa4e67d791
soO <444deph12@gmail.com>
2023-04-13 04:31:31 -0700

    trying to fix our error on app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """app v1 port set to 5050"""
 from flask import Flask, jsonify
-from api.v1.views import app_views
 from models import storage
-import os
+from api.v1.views import app_views
+from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -21,9 +21,7 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 
-if __name__ == "__main__":
-    """ all ips, port 5050"""
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5050))
-
+if __name__ == '__main__':
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,6 @@ from models.user import User
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """Returns a JSON with the status"""
-    from api.v1.views import app_views
     return jsonify({"status": "OK"})
 
 
